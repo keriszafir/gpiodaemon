5cf1d41025456a530fbf2a3870b094cec8c8b840
elegantandrogyne <krzysztof.slychan@gmail.com>
2016-02-15 11:41:55 +0100

    Debug LED blinking problems

--- gpiodaemon/gpiodaemon.py
+++ gpiodaemon/gpiodaemon.py
@@ -19,41 +19,37 @@ led_gpio = 18  # "system ready" LED output
 
 def shutdown():
     """Shut the system down"""
-    ready_led.blink(on_time=0.5, off_time=0.5, n=3)
+    ready_led.blink(on_time=0.5, off_time=0.5, n=3, background=False)
     ready_led.on()
     os.system('shutdown -h now')
 
 
 def reboot():
     """Shut the system down"""
-    ready_led.blink(on_time=0.5, off_time=0.5, n=3)
+    ready_led.blink(on_time=0.5, off_time=0.5, n=3, background=False)
     ready_led.on()
     os.system('shutdown -r now')
 
 
 def gpio_setup():
     """Export the machine cycle sensor GPIO output as file,
-    so that rpi2caster can access it without root privileges:
+    so that rpi2caster can access it without root privileges
     """
+    # Set up sensor GPIO
     os.system('echo "%i" > /sys/class/gpio/export' % sensor_gpio)
     os.system('echo "in" > /sys/class/gpio/gpio%i/direction' % sensor_gpio)
-
-    """Enable generating interrupts on rising and falling edges:"""
+    # Enable generating interrupts on rising and falling edges:
     os.system('echo "both" > /sys/class/gpio/gpio%i/edge' % sensor_gpio)
-
-    """Export the emergency stop button GPIO output as file,
-    so that rpi2caster can access it without root privileges:
-    """
+    # set up emergency stop button GPIO
     os.system('echo "%i" > /sys/class/gpio/export' % emergency_gpio)
     os.system('echo "in" > /sys/class/gpio/gpio%i/direction' % emergency_gpio)
-
-    """Enable generating interrupts on rising and falling edges:"""
+    # Enable generating interrupts on rising and falling edges:"""
     os.system('echo "both" > /sys/class/gpio/gpio%i/edge' % emergency_gpio)
 
 
 def gpio_cleanup():
     """Cleanup after exit"""
-    ready_led.blink(on_time=0.2, off_time=0.2, n=3)
+    ready_led.blink(on_time=0.2, off_time=0.2, n=3, background=False)
     os.system('echo "%i" > /sys/class/gpio/unexport' % sensor_gpio)
     os.system('echo "%i" > /sys/class/gpio/unexport' % emergency_gpio)
 
@@ -65,7 +61,7 @@ def signal_handler(signum, frame):
 
 if __name__ == '__main__':
     # Initialize the LED and buttons
-    ready_led = gpiozero.LED(led_gpio)
+    ready_led = gpiozero.LED(led_gpio, initial_value=True)
     shutdown_button = gpiozero.Button(shutdown_button_gpio, pull_up=True)
     reboot_button = gpiozero.Button(reboot_button_gpio, pull_up=True)
     # Set up a sysfs interface for gpios, guarantee tearing it down on exit
@@ -75,8 +71,6 @@ if __name__ == '__main__':
     except RuntimeError:
         print('You must run this program as root!')
         exit()
-    # Success? Light the LED up
-    ready_led.on()
     # Do nothing (and wait for interrupts from buttons)
     shutdown_button.when_activated = shutdown
     reboot_button.when_activated = reboot
